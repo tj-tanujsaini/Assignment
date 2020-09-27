1d81355493cf7d3b697b7ee566604a19d31b578e
tj.tanujsaini <tj.tanujsaini@gmail.com>
2020-09-27 23:31:31 +0530

    Final Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+//Main App Module handles the starting of the web app
+
+import { Component, OnInit } from '@angular/core';
+import { PaymentDataService } from './payment-display/payment-data.service';
+import { PaymentDetail } from './payment-display/payment.model';
+import { SampleDataService } from './payment-display/sample-data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  
   title = 'FreightFoxAssignment';
+
+  constructor(private paymentService: PaymentDataService,
+              private sampleDataService: SampleDataService){}
+
+  ngOnInit(): void{
+    //try to fetch payment data from local Storage
+    let paymentData: PaymentDetail[] = JSON.parse(window.localStorage.getItem('localPaymentArray'));
+    
+    if(!paymentData){
+      //If paymentData not found on localstorage.. Data will be fetch from SampleDataService()  
+      paymentData = this.sampleDataService.getSampleData();
+    }
+
+    //Sending the paymentData to PaymentDataService() to be used througout the web app
+    this.paymentService.setPaymentData(paymentData);
+
+  }
+
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PaymentDisplayComponent } from './payment-display/payment-display.component';
+import { PaymentEditComponent } from './payment-display/payment-edit/payment-edit.component';
+import { PaymentAddComponent } from './payment-display/payment-add/payment-add.component';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PaymentDisplayComponent,
+    PaymentEditComponent,
+    PaymentAddComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
